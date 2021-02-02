d50e25a761b7a1a356d9b29a1175a77ce859d94b
muga0926 <68101835+muga0926@users.noreply.github.com>
2021-02-02 10:45:10 +0900

    Update setconf.sh

--- executable/setconf.sh
+++ executable/setconf.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+cp ~/settings/dtnd_default.conf ~/settings/dtnd.conf
+
+echo -n "Input host name: "
+read hname
+sed -i "s/\%hostname\%/$hname/" ~/settings/dtnd_default.conf
+
+echo -n "Input log path: "
+read lpath
+sed -i "s/\%logpath\%/$lpath/" ~/settings/dtnd_default.conf
+
+echo -n "Input bundle path: "
+read bpath
+sed -i "s/\%bundlepath\%/$bpath/" ~/settings/dtnd_default.conf
+
+echo -n "Input interface name: "
+read iname
+sed -i "s/\%interfacename\%/$iname/" ~/settings/dtnd_default.conf
