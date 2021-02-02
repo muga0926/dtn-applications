a66e964056e3c6bd6a0224f812710f2e673ed8c8
muga0926 <68101835+muga0926@users.noreply.github.com>
2021-02-02 13:24:00 +0900

    Update setconf.sh

--- executable/setconf.sh
+++ executable/setconf.sh
@@ -4,7 +4,7 @@ cp ~/dtn-test/settings/dtnd_default.conf ~/dtn-test/settings/dtnd.conf
 
 echo -n "Input host name: "
 read hname
-sed -i "s/\%hostname\%/$hname/" ~/settings/dtnd_default.conf
+sed -i "s/\%hostname\%/$hname/" ~/dtn-test/settings/dtnd_default.conf
 
 #echo -n "Input log path: "
 #read lpath
@@ -16,4 +16,4 @@ sed -i "s/\%hostname\%/$hname/" ~/settings/dtnd_default.conf
 
 echo -n "Input interface name: "
 read iname
-sed -i "s/\%interfacename\%/$iname/" ~/settings/dtnd_default.conf
+sed -i "s/\%interfacename\%/$iname/" ~/dtn-test/settings/dtnd_default.conf
